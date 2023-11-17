64f73e412ddf0fde48c4864548d5bd36f82d9303
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2023-11-17 10:34:28 +0200

    Update

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,4 @@
+'use strict';
 function makeTransaction(quantity, pricePerDroid) {
     return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,4 @@
+'use strict';
 function getShippingMessage(country, price, deliveryFee) {
     return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
